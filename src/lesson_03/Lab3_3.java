4efb97f27b5d5ab7e65331dc786b58215f2c2dd6
Phuc Pham <antoniopham1997@gmail.com>
2022-02-25 01:19:48 +0700

    update

--- src/lesson_03/Lab3_3.java
+++ src/lesson_03/Lab3_3.java
@@ -13,8 +13,8 @@ public class Lab3_3 {
                 }
             }
         }
-            for (int ascendIntArray : intArray) {
-                System.out.println(ascendIntArray);
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
         }
     }
 }
