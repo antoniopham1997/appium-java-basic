78794cb60d612d2466f89919f02282a2c2515dd3
Phuc Pham <antoniopham1997@gmail.com>
2022-03-09 01:59:50 +0700

    update

--- src/lesson_06/Lab6_4.java
+++ src/lesson_06/Lab6_4.java
@@ -9,14 +9,6 @@ public class Lab6_4 {
 
         String[] result = url.split("://google");
 
-        if(result[0].equals("http")) {
-            System.out.print("It's http and ");
-        } if(result[0].equals("https")) {
-            System.out.print("It's https and ");
-        } if(result[1].equals(".com")) {
-            System.out.print("domain name is dot com");
-        } if(result[1].equals(".net")) {
-            System.out.print("domain name is dot net");
-        }
+        System.out.println("It's " + result[0] + " and " + "domain name is " + result[1]);
     }
 }
